eaa7c3c68204817d44f38bb029b823f2d9c031c1
ISHANT-GUPTA <ishantg0505@gmail.com>
2025-07-25 01:05:03 +0530

    Added STT Model Class

--- voice/main.py
+++ voice/main.py
@@ -5,16 +5,20 @@ import time
 import torch
 
 torch.cuda.empty_cache()
-model = SeamlessM4Tv2ForSpeechToText.from_pretrained("ai4bharat/indic-seamless", torch_dtype=torch.bfloat16, device_map="cuda")
-processor = SeamlessM4TFeatureExtractor.from_pretrained("ai4bharat/indic-seamless")
-tokenizer = SeamlessM4TTokenizer.from_pretrained("ai4bharat/indic-seamless")
 
-audio, orig_freq = torchaudio.load("./audio.wav")
-audio = torchaudio.functional.resample(audio, orig_freq=orig_freq, new_freq=16_000)
+class STTModel():
+    model = SeamlessM4Tv2ForSpeechToText.from_pretrained("ai4bharat/indic-seamless", torch_dtype=torch.bfloat16, device_map="cuda" if torch.cuda.is_available() else "cpu")
+    processor = SeamlessM4TFeatureExtractor.from_pretrained("ai4bharat/indic-seamless")
+    tokenizer = SeamlessM4TTokenizer.from_pretrained("ai4bharat/indic-seamless")
 
-start = time.perf_counter()
-audio_inputs = processor(audio, sampling_rate=16_000, return_tensors="pt").to("cuda")
-text_out = model.generate(**audio_inputs, tgt_lang="hin")[0].cpu().numpy().squeeze()
-print(tokenizer.decode(text_out, clean_up_tokenization_spaces=True, skip_special_tokens=True))
-end = time.perf_counter()
-print(end-start)
+    @classmethod
+    def transcribe(cls) -> str:
+        audio, orig_freq = torchaudio.load("/home/ishant-gupta/Downloads/input.ogg", format="ogg")
+        audio = torchaudio.functional.resample(audio, orig_freq=orig_freq, new_freq=16_000)
+        start = time.perf_counter()
+        audio_inputs = cls.processor(audio, sampling_rate=16_000, return_tensors="pt").to("cuda")
+        text_out = cls.model.generate(**audio_inputs, tgt_lang="eng")[0].cpu().numpy().squeeze()
+        end = time.perf_counter()
+        print(end-start)
+        return cls.tokenizer.decode(text_out, clean_up_tokenization_spaces=True, skip_special_tokens=True)
+
